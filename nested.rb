697210f34ba181413151c0d49fd77507b5560d51
patrickp246@gmail.com <patrickp246@gmail.com>
2018-09-13 19:57:21 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -108,6 +108,7 @@ def changing_alan
      alans_new_info = "GUI"
      
   programmer_hash[:alan_kay][:known_for] = alans_new_info 
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -126,5 +127,5 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash[:dennis_ritchie][:languages].push("Assembly")
 end
